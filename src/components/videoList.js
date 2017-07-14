c0bde2be575a2071df78b66acc3c4455daae7309
JR Gray <wgray@tuta.io>
2017-07-14 14:56:45 -0700

    Render example video data in videoList

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,8 @@
 angular.module('video-player')
+.controller('videoListController', function($scope) {
+	$scope.videos = window.exampleVideoData;
+})
 .component('videoList', {
-  templateUrl: 'src/templates/videoList.html'
+  templateUrl: 'src/templates/videoList.html',
+  conroller: 'videoListController'
 });
